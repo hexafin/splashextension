5b51b2d8562eaf197f974060ba9b5a9693d6be45
Daniel Wyb <danielwyb@icloud.com>
2018-04-22 15:09:03 -0400

    Add icon closes #33

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,47 +1,50 @@
-import gulp from 'gulp';
-import loadPlugins from 'gulp-load-plugins';
-import webpack from 'webpack';
-import rimraf from 'rimraf';
+import gulp from 'gulp'
+import loadPlugins from 'gulp-load-plugins'
+import webpack from 'webpack'
+import rimraf from 'rimraf'
 
-const plugins = loadPlugins();
+const plugins = loadPlugins()
 
-import eventWebpackConfig from './event/webpack.config';
-import contentWebpackConfig from './content/webpack.config';
+import eventWebpackConfig from './event/webpack.config'
+import contentWebpackConfig from './content/webpack.config'
 
-gulp.task('event-js', ['clean'], (cb) => {
+gulp.task('event-js', ['clean'], cb => {
   webpack(eventWebpackConfig, (err, stats) => {
-    if(err) throw new plugins.util.PluginError('webpack', err);
+    if (err) throw new plugins.util.PluginError('webpack', err)
 
-    plugins.util.log('[webpack]', stats.toString());
+    plugins.util.log('[webpack]', stats.toString())
 
-    cb();
-  });
-});
+    cb()
+  })
+})
 
-gulp.task('content-js', ['clean'], (cb) => {
+gulp.task('content-js', ['clean'], cb => {
   webpack(contentWebpackConfig, (err, stats) => {
-    if(err) throw new plugins.util.PluginError('webpack', err);
+    if (err) throw new plugins.util.PluginError('webpack', err)
 
-    plugins.util.log('[webpack]', stats.toString());
+    plugins.util.log('[webpack]', stats.toString())
 
-    cb();
-  });
-});
+    cb()
+  })
+})
 
 gulp.task('copy-manifest', ['clean'], () => {
-  return gulp.src('manifest.json')
-    .pipe(gulp.dest('./build'));
-});
+  return gulp.src('manifest.json').pipe(gulp.dest('./build'))
+})
 
-gulp.task('clean', (cb) => {
-  rimraf('./build', cb);
-});
+gulp.task('copy-assets', ['clean'], () => {
+  return gulp.src('./assets/*').pipe(gulp.dest('./build'))
+})
 
-gulp.task('build', ['copy-manifest', 'event-js', 'content-js']);
+gulp.task('clean', cb => {
+  rimraf('./build', cb)
+})
+
+gulp.task('build', ['copy-manifest', 'copy-assets', 'event-js', 'content-js'])
 
 gulp.task('watch', ['default'], () => {
-  gulp.watch('content/**/*', ['build']);
-  gulp.watch('event/**/*', ['build']);
-});
+  gulp.watch('content/**/*', ['build'])
+  gulp.watch('event/**/*', ['build'])
+})
 
-gulp.task('default', ['build']);
+gulp.task('default', ['build'])
